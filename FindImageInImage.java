d27dd7a19eab645adad97cbbea5793540404cff5
Clemens Anhuth <clemens@froglogic.com>
2016-02-23 02:26:31 +0100

    Add too many changes at once, arf

--- FindImageInImage.java
+++ FindImageInImage.java
@@ -1,11 +1,13 @@
 import java.awt.image.BufferedImage;
+import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.io.File;
+import java.util.ArrayList;
 import javax.imageio.ImageIO;
 
-public class FindImageInImage {
+public class FindImageInImage extends Thread {
     static final int versionMajor = 1;
     static final int versionMinor = 0;
 
@@ -18,8 +20,64 @@ public class FindImageInImage {
 
     static String filename1 = null;
     static String filename2 = null;
-    static int allowedPixelColorDifference = 0;
-    static double allowedPixelFailsPercent = 0.0;
+
+    static BufferedImage image = null;
+    static BufferedImage subImage = null;
+
+    static double allowedPixelFailsPercent;
+    static int allowedPixelColorDifference;
+
+    static int userDesiredThreadCount = Runtime.getRuntime().availableProcessors();
+
+    private int startAtY;
+    private int stopAtYInclusive;
+    private boolean subImageFound = false;
+    private ArrayList<Point> foundAtPoint = new ArrayList<Point>();
+    private ArrayList<Integer> foundFailedPixelCount = new ArrayList<Integer>();
+    private ArrayList<Double> foundFailedPixelPercent = new ArrayList<Double>();
+
+    public FindImageInImage(
+            int startAtY,
+            int stopAtYInclusive,
+            double allowedPixelFailsPercent,
+            int allowedPixelColorDifference ) {
+        super();
+        this.startAtY = startAtY;
+        this.stopAtYInclusive = stopAtYInclusive;
+        this.allowedPixelFailsPercent = allowedPixelFailsPercent;
+        this.allowedPixelColorDifference = allowedPixelColorDifference;
+    }
+
+    public void run() {
+        if ( findSubImage(
+                subImage,
+                image,
+                startAtY,
+                stopAtYInclusive,
+                allowedPixelFailsPercent,
+                allowedPixelColorDifference,
+                foundAtPoint,
+                foundFailedPixelCount,
+                foundFailedPixelPercent ) ) {
+            subImageFound = true;
+        }
+    }
+
+    public boolean subImageFound() {
+        return this.subImageFound;
+    }
+
+    public ArrayList<Point> getFoundAtPoint() {
+        return this.foundAtPoint;
+    }
+
+    public ArrayList<Integer> getFoundFailedPixelCount() {
+        return this.foundFailedPixelCount;
+    }
+
+    public ArrayList<Double> getFoundFailedPixelPercent() {
+        return this.foundFailedPixelPercent;
+    }
 
     public static void main( String[] args ) throws Exception {
         if ( args.length == 0 ) {
@@ -28,81 +86,148 @@ public class FindImageInImage {
         }
 
         handleArgs(args);
-        BufferedImage subImage = readImage( filename1 );
-        BufferedImage img = null;
+        subImage = readImage( filename1 );
+        image = null;
         if ( filename2 == null ) {
-            img = new Robot().createScreenCapture( new Rectangle( Toolkit.getDefaultToolkit().getScreenSize() ) );
+            image = new Robot().createScreenCapture( new Rectangle( Toolkit.getDefaultToolkit().getScreenSize() ) );
         } else {
-            img = readImage( filename2 );
+            image = readImage( filename2 );
         }
 
-        if ( subImage.getWidth() > img.getWidth() ) {
+        if ( subImage.getWidth() > image.getWidth() ) {
             println( "ERROR: Sub image width is larger than image width." );
             System.exit( EXIT_CODE_SUB_IMAGE_WIDTH_TOO_LARGE );
-        } else if ( subImage.getHeight() > img.getHeight() ) {
+        } else if ( subImage.getHeight() > image.getHeight() ) {
             println( "ERROR: Sub image height is larger than image height." );
             System.exit( EXIT_CODE_SUB_IMAGE_HEIGHT_TOO_LARGE );
         }
 
-        if ( findSubImage(
-                subImage,
-                img,
+        println( "SUB_IMAGE_WIDTH=" + subImage.getWidth() );
+        println( "SUB_IMAGE_HEIGHT=" + subImage.getHeight() );
+        println( "SUB_IMAGE_TOTAL_PIXELS=" + subImage.getWidth() * subImage.getHeight() );
+        println( "IMAGE_TOTAL_PIXELS=" + image.getWidth() * image.getHeight() );
+        println( "ALLOWED_PIXEL_FAILS_COUNT=" + (int)( ( (double)( subImage.getWidth() * subImage.getHeight() ) / 100.0 ) * allowedPixelFailsPercent ) );
+
+        int heightDiff = image.getHeight() - subImage.getHeight();
+        int threadCount = userDesiredThreadCount;
+        FindImageInImage[] threads = new FindImageInImage[ threadCount ];
+        int heightPerCPU = heightDiff / threadCount;
+        // threadCount: 2
+        // image height: 100
+        // subimage height: 20
+        // heightDiff: 80
+        // heightPerCPU: 40
+        // Thread 1: 1*40 -> 40*(1+1)
+        //  40, 80
+        // Thread 0: 0, 40-1
+
+        // Start at 1; the first range will be handled in the initial
+        // thread
+        for ( int i = 0; i < threadCount; i++ ) {
+            int startAtY = heightPerCPU * i;
+            int stopAtYInclusive = ( heightPerCPU * ( i + 1 ) ) - 1;
+            // Make sure to cover up to image.getHeight() in
+            // last thread:
+            if ( i == threadCount - 1 ) {
+                stopAtYInclusive = heightDiff;
+            }
+            threads[ i ] = new FindImageInImage(
+                startAtY,
+                stopAtYInclusive,
                 allowedPixelFailsPercent,
-                allowedPixelColorDifference ) ) {
-            System.exit( EXIT_CODE_IMAGE_FOUND );
+                allowedPixelColorDifference );
+            threads[ i ].setName( "Thread #" + ( i + 1 ) );
         }
 
-        System.exit( EXIT_CODE_IMAGE_NOT_FOUND );
+        long startMillis = System.currentTimeMillis();
+
+        for ( int i = 0; i < threads.length; i++ ) {
+            threads[ i ].start();
+            //threads[ i ].join();
+        }
+
+        boolean subImageFound = false;
+        int occurrence = 1;
+        for ( int i = 0; i < threads.length; i++ ) {
+            FindImageInImage t = threads[ i ];
+            t.join();
+            if ( !t.subImageFound() ) {
+                continue;
+            }
+            if ( !subImageFound ) {
+                subImageFound = true;
+                println( "FOUND=YES" );
+            }
+            ArrayList<Point> foundAtPoint = t.getFoundAtPoint();
+            ArrayList<Integer> foundFailedPixelCount = t.getFoundFailedPixelCount();
+            ArrayList<Double> foundFailedPixelPercent = t.getFoundFailedPixelPercent();
+            for ( int i2 = 0; i2 < foundAtPoint.size(); i2++ ) {
+                Point p = foundAtPoint.get( i2 );
+                println( "FOUND_" + ( i2 + 1 ) + "_AT_X=" + (int)p.getX() );
+                println( "FOUND_" + ( i2 + 1 ) + "_AT_Y=" + (int)p.getY() );
+                println( "FOUND_" + ( i2 + 1 ) + "_FAILED_PIXEL_COUNT=" + foundFailedPixelCount.get( i2 ) );
+                println( "FOUND_" + ( i2 + 1 ) + "_FAILED_PIXEL_PERCENT=" + foundFailedPixelPercent.get( i2 ) );
+            }
+        }
+
+        println( "RUN_TIME_MILLIS=" + ( System.currentTimeMillis() - startMillis ) );
+        if ( subImageFound ) {
+            System.exit( EXIT_CODE_IMAGE_FOUND );
+        } else {
+            println( "FOUND=NO" );
+            System.exit( EXIT_CODE_IMAGE_NOT_FOUND );
+        }
     }
 
     static boolean findSubImage(
             BufferedImage subImage,
-            BufferedImage img,
+            BufferedImage image,
+            int startAtY,
+            int stopAtYInclusive,
             double allowedPixelFailsPercent,
-            int allowedPixelColorDifference ) {
+            int allowedPixelColorDifference,
+            ArrayList<Point> foundAtPoint,
+            ArrayList<Integer> foundFailedPixelCount,
+            ArrayList<Double> foundFailedPixelPercent ) {
+
+        //println( Thread.currentThread().getName() + ": startAtY: " + startAtY + ", stopAtYInclusive: " + stopAtYInclusive );
         int subWidth = subImage.getWidth();
         int subHeight = subImage.getHeight();
-        println( "SUB_IMAGE_TOTAL_PIXELS=" + subWidth * subHeight );
-
-        int width = img.getWidth();
-        int height = img.getHeight();
-        println( "IMAGE_TOTAL_PIXELS=" + width * height );
+        int width = image.getWidth();
+        int height = image.getHeight();
         int allowedPixelFailsCount = (int)( ( (double)( subWidth * subHeight) / 100.0 ) * allowedPixelFailsPercent );
-        println( "ALLOWED_PIXEL_FAILS_COUNT=" + allowedPixelFailsCount );
-        long startMillis = System.currentTimeMillis();
         int xOffsetMax = width - subWidth;
         for ( int xOffset = 0; xOffset <= xOffsetMax; xOffset++ ) {
-            int yOffsetMax = height - subHeight;
-            for ( int yOffset = 0; yOffset <= yOffsetMax; yOffset++ ) {
-                if ( subImageIsAtOffset(
+            for ( int yOffset = startAtY; yOffset <= stopAtYInclusive; yOffset++ ) {
+                int[] returnActualPixelFailsCount = new int[ 1 ];
+                if ( isSubImageAtOffset(
                         subImage,
-                        img,
+                        image,
                         xOffset,
                         yOffset,
                         allowedPixelFailsCount,
-                        allowedPixelColorDifference ) ) {
-                    println( "FOUND=YES" );
-                    println( "FOUND_AT_X=" + xOffset );
-                    println( "FOUND_AT_Y=" + yOffset );
-                    println( "RUN_TIME_MILLIS=" + ( System.currentTimeMillis() - startMillis ) );
-                    return true;
+                        allowedPixelColorDifference,
+                        returnActualPixelFailsCount ) ) {
+                    foundAtPoint.add( new Point( xOffset, yOffset ) );
+                    foundFailedPixelCount.add( returnActualPixelFailsCount[ 0 ] );
+                    double p = returnActualPixelFailsCount[ 0 ] / ( (double)subWidth * (double)subHeight / 100.0 );
+                    foundFailedPixelPercent.add( p );
                 }
             }
         }
-        println( "FOUND=NO" );
-        println( "RUN_TIME_MILLIS=" + ( System.currentTimeMillis() - startMillis ) );
-        return false;
+        return foundAtPoint.size() > 0;
     }
 
-    static boolean subImageIsAtOffset(
+    static boolean isSubImageAtOffset(
             BufferedImage subImage,
-            BufferedImage img,
+            BufferedImage image,
             int xOffset,
             int yOffset,
             int allowedPixelFailsCount,
-            int allowedPixelColorDifference ) {
-        int width = img.getWidth();
-        int height = img.getHeight();
+            int allowedPixelColorDifference,
+            int[] returnActualPixelFailsCount ) {
+        int width = image.getWidth();
+        int height = image.getHeight();
         int subWidth = subImage.getWidth();
         int subHeight = subImage.getHeight();
         int actualPixelFailsCount = 0;
@@ -125,10 +250,10 @@ public class FindImageInImage {
 
                 int x = xOffset + subImageX;
                 int y = yOffset + subImageY;
-                int imgPixel = img.getRGB( x, y );
-                r = ( imgPixel >> 0 ) & 0x000000ff;
-                g = ( imgPixel >> 1 ) & 0x000000ff;
-                b = ( imgPixel >> 2 ) & 0x000000ff;
+                int imagePixel = image.getRGB( x, y );
+                r = ( imagePixel >> 0 ) & 0x000000ff;
+                g = ( imagePixel >> 1 ) & 0x000000ff;
+                b = ( imagePixel >> 2 ) & 0x000000ff;
                 int v2 = r + g + b;
                 int colorDifference = Math.abs( v1 - v2 );
                 if ( colorDifference > allowedPixelColorDifference ) {
@@ -139,9 +264,7 @@ public class FindImageInImage {
                 }
             }
         }
-        println( "FAILED_PIXEL_COUNT=" + actualPixelFailsCount );
-        double p = actualPixelFailsCount / ( (double)subWidth * (double)subHeight / 100.0 );
-        println( "FAILED_PIXEL_PERCENT=" + p );
+        returnActualPixelFailsCount[ 0 ] = actualPixelFailsCount;
         return true;
     }
 
@@ -179,13 +302,20 @@ public class FindImageInImage {
         println();
         println("OPTIONS:");
         println();
-        println(" --allowed-pixel-color-difference");
+        println(" --allowed-pixel-color-difference=x");
         println("   Allowed pixel color difference.");
         println("   An integer value.");
         println();
-        println(" --allowed-pixel-fails-percent");
+        println(" --allowed-pixel-fails-percent=x");
         println("   Allowed percentage of pixel color mismatches.");
         println("   An integer or floating point value.");
+        println();
+        println(" --thread-count=x");
+        println("   Number of threads to use.");
+        println("   An integer value.");
+        println("   Defaults to number of processors reported by JRE.");
+        println("   If specified value is larger than number of");
+        println("   processors reported by JRE, default to the latter.");
         println();
         println( "EXIT CODES:" );
         println();
@@ -208,28 +338,29 @@ public class FindImageInImage {
             } else if ( arg.equals( "--version" ) ) {
                 printVersion();
                 System.exit( 0 );
-            } else if ( arg.equals( "--allowed-pixel-color-difference" ) ) {
-                if ( argIndex == args.length -1 ) {
-                    println( "ERROR: Missing value for parameter: " + arg );
-                    System.exit( EXIT_CODE_INVALID_ARGUMENTS );
-                }
+            } else if ( arg.indexOf( "--allowed-pixel-color-difference=" ) != -1 ) {
                 try {
-                    argIndex++;
-                    allowedPixelColorDifference = Integer.parseInt( args[ argIndex ] );
+                    allowedPixelColorDifference = Integer.parseInt( arg.split( "=", 2 )[ 1 ] );
                 } catch ( Exception e ) {
                     println( "ERROR: Invalid value for parameter: " + arg );
                     println();
                     printUsage();
                     System.exit( EXIT_CODE_INVALID_ARGUMENTS );
                 }
-            } else if ( arg.equals( "--allowed-pixel-fails-percent" ) ) {
-                if ( argIndex == args.length -1 ) {
-                    println( "ERROR: Missing value for parameter: " + arg );
-                    System.exit( EXIT_CODE_INVALID_ARGUMENTS );
-                }
+            } else if ( arg.indexOf( "--allowed-pixel-fails-percent=" ) != -1 ) {
                 try {
-                    argIndex++;
-                    allowedPixelFailsPercent = Double.parseDouble( args[ argIndex ] );
+                    allowedPixelFailsPercent = Double.parseDouble( arg.split( "=", 2 )[ 1 ] );
+                } catch ( Exception e ) {
+                    println( "ERROR: Invalid value for parameter: " + arg );
+                    printUsage();
+                    System.exit( EXIT_CODE_INVALID_ARGUMENTS);
+                }
+            } else if ( arg.indexOf( "--threads=" ) != -1 ) {
+                try {
+                    userDesiredThreadCount = Integer.parseInt( arg.split( "=", 2 )[ 1 ] );
+                    if ( userDesiredThreadCount <= 0 ) {
+                        throw new Exception();
+                    }
                 } catch ( Exception e ) {
                     println( "ERROR: Invalid value for parameter: " + arg );
                     printUsage();
--- run.bat
+++ run.bat
@@ -3,10 +3,19 @@ if "%~1" neq "" java FindImageInImage %*
 if "%~1" neq "" goto :eof
 
 ::java FindImageInImage test_image_sub_1px_diff.png test_image.png
+java FindImageInImage test_image_sub.png
+echo.
+::java FindImageInImage --allowed-pixel-fails-percent=0.5 test_image_sub_1px_diff.png
 ::java FindImageInImage test_image_sub.png test_image.png
-::java FindImageInImage --allowed-pixel-fails-percent 1 test_image_sub_1px_diff.png test_image.png
-::java FindImageInImage --allowed-pixel-color-difference 0 --allowed-pixel-fails-percent 50 test_image_sub_small_color_diffs.png test_image_sub.png
+::java FindImageInImage test_image_sub.png test_image_sub.png
+::java FindImageInImage --allowed-pixel-fails-percent=1 test_image_sub_1px_diff.png test_image.png
+::java FindImageInImage --allowed-pixel-color-difference=0 --allowed-pixel-fails-percent=50 test_image_sub_small_color_diffs.png test_image_sub.png
 
-java FindImageInImage --allowed-pixel-color-difference 150 --allowed-pixel-fails-percent 24.5 test_image_sub_small_color_diffs.png test_image_sub.png
+::java FindImageInImage --allowed-pixel-color-difference=150 --allowed-pixel-fails-percent=24.5 test_image_sub_small_color_diffs.png test_image_sub.png
 
-java FindImageInImage --allowed-pixel-color-difference 150 --allowed-pixel-fails-percent 24.5 test_image_sub_small_color_diffs.png test_image.png
+::java FindImageInImage --allowed-pixel-color-difference=150 --allowed-pixel-fails-percent=24.5 test_image_sub_small_color_diffs.png test_image.png
+
+java FindImageInImage --threads=1 --allowed-pixel-color-difference=100 --allowed-pixel-fails-percent=10.5 test_image_sub.png
+echo.
+
+java FindImageInImage --allowed-pixel-color-difference=100 --allowed-pixel-fails-percent=10.5 test_image_sub.png
